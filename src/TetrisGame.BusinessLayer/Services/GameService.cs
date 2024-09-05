7fd246606496e7ed3dc6a2ab0a5a6873d3ff6f63
N1K0232 <ns.nicolasilvestri@libero.it>
2024-09-05 22:01:42 +0200

    Add service to manage the game

---
+++ src/TetrisGame.BusinessLayer/Services/GameService.cs
@@ -0,0 +1,127 @@
+﻿using TetrisGame.BusinessLayer.Services.Interfaces;
+using TetrisGame.Shared;
+using TetrisGame.Shared.Models.Common;
+
+namespace TetrisGame.BusinessLayer.Services;
+
+public class GameService : IGameService
+{
+    private Block currentBlock;
+    private bool gameOver;
+
+    public GameService()
+    {
+        GameGrid = new GameGrid(22, 10);
+        BlockQueue = new BlockQueue();
+        CurrentBlock = BlockQueue.GetBlockAndUpdate();
+    }
+
+    public Block CurrentBlock
+    {
+        get
+        {
+            return currentBlock;
+        }
+        private set
+        {
+            if (value != currentBlock)
+            {
+                currentBlock = value;
+                currentBlock.Reset();
+            }
+        }
+    }
+
+    public GameGrid GameGrid { get; }
+
+    public BlockQueue BlockQueue { get; }
+
+    public bool GameOver => gameOver;
+
+    public void RotateBlockCW()
+    {
+        CurrentBlock.RotateCW();
+
+        if (!BlockFits())
+        {
+            CurrentBlock.RotateCCW();
+        }
+    }
+
+    public void RotateBlockCCW()
+    {
+        CurrentBlock.RotateCCW();
+
+        if (!BlockFits())
+        {
+            CurrentBlock.RotateCW();
+        }
+    }
+
+    public void MoveBlockLeft()
+    {
+        CurrentBlock.Move(0, -1);
+
+        if (!BlockFits())
+        {
+            CurrentBlock.Move(0, 1);
+        }
+    }
+
+    public void MoveBlockRight()
+    {
+        CurrentBlock.Move(0, 1);
+
+        if (!BlockFits())
+        {
+            CurrentBlock.Move(0, -1);
+        }
+    }
+
+    public void MoveBlockDown()
+    {
+        CurrentBlock.Move(1, 0);
+
+        if (!BlockFits())
+        {
+            CurrentBlock.Move(-1, 0);
+            PlaceBlock();
+        }
+    }
+
+    private bool IsGameOver()
+    {
+        return !(GameGrid.IsRowEmpty(0) && GameGrid.IsRowEmpty(1));
+    }
+
+    private bool BlockFits()
+    {
+        foreach (Position p in CurrentBlock.GetTilePositions())
+        {
+            if (!GameGrid.IsEmpty(p.Row, p.Column))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void PlaceBlock()
+    {
+        foreach (Position p in CurrentBlock.GetTilePositions())
+        {
+            GameGrid[p.Row, p.Column] = CurrentBlock.Id;
+        }
+
+        GameGrid.ClearFullRows();
+        if (IsGameOver())
+        {
+            gameOver = true;
+        }
+        else
+        {
+            CurrentBlock = BlockQueue.GetBlockAndUpdate();
+        }
+    }
+}
---
+++ src/TetrisGame.BusinessLayer/Services/Interfaces/IGameService.cs
@@ -0,0 +1,17 @@
+﻿using TetrisGame.Shared;
+using TetrisGame.Shared.Models.Common;
+
+namespace TetrisGame.BusinessLayer.Services.Interfaces;
+public interface IGameService
+{
+    BlockQueue BlockQueue { get; }
+    Block CurrentBlock { get; }
+    GameGrid GameGrid { get; }
+    bool GameOver { get; }
+
+    void MoveBlockDown();
+    void MoveBlockLeft();
+    void MoveBlockRight();
+    void RotateBlockCCW();
+    void RotateBlockCW();
+}
